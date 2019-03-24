adccecb7fb02f4c36c47f4dec8b4118425b550d3
ShunHub <734244502@qq.com>
2019-03-24 14:21:41 +0800

    add tree

--- DSALL/Tree2/Tree.h
+++ DSALL/Tree2/Tree.h
@@ -21,6 +21,86 @@ public:
 		pRChild = NULL;
 		pParent = NULL;
 	}
+
+	Node *SearchNode(int nodeIndex) {
+
+		if (this->index == nodeIndex)
+			return this;
+
+		Node *temp = NULL;
+
+		if (this->pLChild != NULL) {
+			if (this->pLChild->index == nodeIndex)
+				return this->pLChild;
+			else{
+				temp = this->pLChild->SearchNode(nodeIndex);
+				if (temp != NULL)
+					return temp;
+			}
+		}
+
+		if (this->pRChild != NULL) {
+			if (this->pRChild->index == nodeIndex)
+				return this->pRChild;
+			else {
+				temp = this->pRChild->SearchNode(nodeIndex);
+				if (temp != NULL)
+					return temp;
+			}
+		}
+		
+		return NULL;
+	}
+
+	void DeleteNode() {
+		if (this->pLChild != NULL)
+			this->pLChild->DeleteNode();
+		if (this->pRChild != NULL)
+			this->pRChild->DeleteNode();
+		
+		if (this->pParent != NULL) {
+			if (this->pParent->pLChild == this)
+				this->pParent->pLChild = NULL;
+			else if (this->pParent->pRChild == this)
+				this->pParent->pRChild = NULL;
+		}
+
+		delete this;
+
+	}
+
+
+	void PreorderTraversal() {
+
+		cout << this->index << "   :   " << this->data << endl;
+		if (this->pLChild != NULL)
+			this->pLChild->PreorderTraversal();
+		if (this->pRChild != NULL)
+			this->pRChild->PreorderTraversal();
+	}
+
+	void InorderTraversal() {
+		if (this->pLChild != NULL)
+			this->pLChild->InorderTraversal();
+
+		cout << this->index << "   :   " << this->data << endl;
+
+		if (this->pRChild != NULL)
+			this->pRChild->InorderTraversal();
+	}
+
+	void PostorderTraversal() {
+		if (this->pLChild != NULL)
+			this->pLChild->PostorderTraversal();
+
+		if (this->pRChild != NULL)
+			this->pRChild->PostorderTraversal();
+		cout << this->index << "   :   " << this->data << endl;
+	}
+
+
+
+
 };
 
 class Tree {
@@ -32,7 +112,69 @@ public:
 	Tree() {
 		m_pRoot = new Node();
 	}
-	~Tree() { delete m_pRoot; }
+
+	~Tree() {
+		m_pRoot->DeleteNode();
+	}
+
+
+	Node *SearchNode(int nodeIndex) {
+		return m_pRoot->SearchNode(nodeIndex);
+	}
+
+	bool AddNode(int nodeIndex, int direction, Node *pNode) {
+
+		Node *temp = SearchNode(nodeIndex);
+		if (temp == NULL)
+			return false;
+
+		Node * node = new Node();
+		node->index = pNode->index;
+		node->data = pNode->data;
+		node->pParent = temp;
+
+		if (direction == 0) {
+
+			temp->pLChild = node;
+			return true;
+
+		}
+		else if (direction == 1) {
+			temp->pRChild = node;
+			return true;
+		}
+		return false;
+	}
+
+	bool DeleteNode(int nodeIndex, Node *pNode) {
+
+		Node *temp = SearchNode(nodeIndex);
+		if (temp == NULL)
+			return false;
+
+		if (pNode != NULL) 
+			pNode->data = temp->data;
+
+		temp->DeleteNode();
+		return true;
+
+	}
+
+
+	//前序遍历
+	void PreorderTraversal() {
+		m_pRoot->PreorderTraversal();
+	}
+
+	//中序遍历
+	void InorderTraversal() {
+		m_pRoot->InorderTraversal();
+	}
+
+	//后序遍历
+	void PostorderTraversal() {
+		m_pRoot->PostorderTraversal();
+	}
 
 
 
--- DSALL/Tree2/main.cpp
+++ DSALL/Tree2/main.cpp
@@ -5,7 +5,59 @@
 using std::cout; using std::cin; using std::endl;
 
 
+
+
+
+
+
+
 int main(int argc, char **argv) {
+
+
+	Tree *tree = new Tree();
+
+Node *node1 = new Node();
+node1->index = 1;
+node1->data = 5;
+
+Node *node2 = new Node();
+node2->index = 2;
+node2->data = 8;
+
+Node *node3 = new Node();
+node3->index = 3;
+node3->data = 2;
+
+Node *node4 = new Node();
+node4->index = 4;
+node4->data = 6;
+
+Node *node5 = new Node();
+node5->index = 5;
+node5->data = 9;
+
+Node *node6 = new Node();
+node6->index = 6;
+node6->data = 7;
+
+
+tree->AddNode(0, 0, node1);
+tree->AddNode(0, 1, node2);
+tree->AddNode(1, 0, node3);
+tree->AddNode(1, 1, node4);
+tree->AddNode(2, 0, node5);
+tree->AddNode(2, 1, node6);
+
+tree->DeleteNode(2, NULL);
+
+
+
+
+tree->PreorderTraversal();
+//tree->InorderTraversal();
+//tree->PostorderTraversal();  
+
+
 
 
 	cin.get();
